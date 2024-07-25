21f26878b0473eb05a60979b2909e110fbf5b68b
EzioSweet <cn.djx@outlook.com>
2024-07-25 23:34:10 +0800

    完成

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "github.com/alecthomas/kingpin/v2"
     "github.com/go-zoox/fetch"
+    "os"
 )
 
 var (
@@ -22,7 +23,8 @@ func main() {
 		fmt.Printf("使用远程配置文件地址 %s\n",*remote)
 		getRemoteConfig(*remote)
 	}else {
-		fmt.Printf("使用远程配置文件地址 %s\n",*local)
+		fmt.Printf("使用本地配置文件地址 %s\n",*local)
+		fmt.Println("还未实现，敬请期待")
 	}
 }
 
@@ -37,23 +39,79 @@ func getRemoteConfig(url string){
     if err != nil {
         panic(err) 
     }
-    for _, element := range config.Modrinth {
-		if element.Version=="" {
-			modrinthUrl := fmt.Sprintf(
-				"https://api.modrinth.com/v2/project/%s/version?game_versions=[\"%s\"]&loaders=[\"fabric\"]",
-				element.Name,config.McVersion)
-			resp ,err = fetch.Get(modrinthUrl)
-			modrinth := new([]model.ModrinthModel)
-            err = resp.UnmarshalJSON(modrinth)
-            if err != nil {
-                panic(err) 
-            }
-			resp,err = fetch.Download((*modrinth)[0].Files[0].Url,"./mods/"+(*modrinth)[0].Files[0].FileName)
+	if config.Loader==""{
+		fmt.Println("请配置mod加载器类型")
+		return
+	}
+	fmt.Println()
+	fmt.Println("1.开始Modrinth Mod下载")
+	loader:=config.Loader
+    for i, element := range config.Modrinth {
+		loader = config.Loader
+
+		if element.Loader != ""{
+			loader = element.Loader
+		}
+		modrinthUrl := fmt.Sprintf(
+			"https://api.modrinth.com/v2/project/%s/version?game_versions=[\"%s\"]&loaders=[\"%s\"]",
+			element.Name,config.McVersion,loader)
+		resp ,err = fetch.Get(modrinthUrl)
+		modrinth := *new([]model.ModrinthModel)
+		err = resp.UnmarshalJSON(&modrinth)
+		if err != nil {
+			panic(err)
+		}
+		if element.Version == ""{
+			fmt.Printf("\r[%d/%d] Downloading %s",i+1, len(config.Modrinth),(modrinth)[0].Files[0].FileName)
+			if element.Path!=""{
+				resp,err = fetch.Download(modrinth[0].Files[0].Url,element.Path)
+			}else{
+				resp,err = fetch.Download(modrinth[0].Files[0].Url,"./mods/"+modrinth[0].Files[0].FileName)
+			}
 			if err != nil {
-				
+				panic(err)
 			}
 		}else{
-			
+			for _,item :=range modrinth{
+                if item.Version == element.Version {
+					fmt.Printf("\r[%d/%d] Downloading %s",i+1, len(config.Modrinth),item.Files[0].FileName)
+					if element.Path!=""{
+						resp,err = fetch.Download(item.Files[0].Url,element.Path)
+					}else{
+						resp,err = fetch.Download(item.Files[0].Url,"./mods/"+item.Files[0].FileName)
+					}
+					break
+                }
+			}
 		}
-    }
+	}
+	fmt.Println()
+	fmt.Println("OK")
+	fmt.Println()
+	fmt.Println("2.开始Local Mod或者Config下载")
+	for _,item := range config.Local{
+		for i,element := range item.List{
+			fmt.Printf("\r[%d/%d] Downloading %s",i+1, len(item.List),element.Url)
+			resp,err = fetch.Download(item.Prefix + element.Url,element.Path)
+			if err !=nil {
+				panic(err)
+			}
+		}
+	}
+	fmt.Println()
+	fmt.Println("OK")
+	fmt.Println()
+	fmt.Println("3.删除无用Mod或者Config")
+
+	for i,element :=range config.Remove{
+		fmt.Printf("\r[%d/%d] Removing %s",i+1, len(config.Remove),element)
+        err = os.Remove(element)
+        if err != nil {
+            panic(err)
+        }
+	}
+	fmt.Println()
+	fmt.Println("OK")
+	fmt.Println()
+	fmt.Println("运行完成")
 }
--- model/model.go
+++ model/model.go
@@ -3,15 +3,20 @@ package model
 type ConfigModel struct {
     Version string `yaml:"version"`
 	McVersion string `yaml:"mcVersion"`
+	Loader string `yaml:"loader"`
 	Modrinth []struct{
 		Name string `yaml:"name"`
 		Path string `yaml:"path"`
+		Loader string `yaml:"loader"`
 		Version string `yaml:"version"`
 	} `yaml:"modrinth"`
 	Local []struct{
-		Url string `yaml:"url"`
-		Path string `yaml:"path"`
-	}
+		Prefix string `yaml:"prefix"`
+		List []struct{
+			Url string `yaml:"url"`
+			Path string `yaml:"path"`
+		} `yaml:"list"`
+	}`yaml:"local"`
 	Remove []string `yaml:"remove"`
 	
 }
